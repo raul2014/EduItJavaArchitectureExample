8c72be1215354358eab75c9fd461014078d3f304
raul_jorge_coronado <raul_jorge_coronado@yahoo.com.ar>
2018-12-22 13:50:43 -0300

    First commit

---
+++ src/main/java/com/eduit/App.java
@@ -0,0 +1,17 @@
+package com.eduit;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        final String nombre = StringUtils.capitalize("rjc");
+        System.out.println(nombre);
+    }
+}
